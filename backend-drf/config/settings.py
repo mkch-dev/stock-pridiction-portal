55bf56833a5d84e3f0be973f7923810360b14832
mkch-dev <khalili.mehdi.6@proton.me>
2025-08-21 23:31:25 +0900

    migration, accounts app and api app installed

--- backend-drf/config/settings.py
+++ backend-drf/config/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'accounts',
+    'api',
+
+    # 3rd party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

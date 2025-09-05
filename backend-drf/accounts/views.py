24fac6694f07888a64a94cf7d7282c1243f6ccf6
mkch-dev <khalili.mehdi.6@proton.me>
2025-09-05 23:42:08 +0900

    protected view

--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
@@ -11,3 +14,13 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        response = {
+            'status': 'Request was authenticated successfully',
+        }
+        return Response(response)

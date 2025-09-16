8f1ee4bd5097bd2c58fe72f3ac9871c57e340311
mkch-dev <khalili.mehdi.6@proton.me>
2025-09-17 00:02:32 +0900

    request Interceptor

--- frontend-react/src/axiosInstance.js
+++ frontend-react/src/axiosInstance.js
@@ -4,12 +4,22 @@ const baseURL = import.meta.env.VITE_BACKEND_BASE_API
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+    }
 })
 
 // Request Interceptor
 axiosInstance.interceptors.request.use(
     function (config) {
+        const accessToken = localStorage.getItem('accessToken')
+        if (accessToken) {
+            config.headers['Authorization'] = `Bearer ${accessToken}`
+        }
         return config
+    },
+    function (error) {
+        return Promise.reject(error)
     }
 )
 
